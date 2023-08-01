aed9d8f919b76599ac111bc004dd957a6f2d8e21
BuildTools <raar518@aucklanduni.ac.nz>
2023-08-02 04:40:55 +1200

    added navigation bar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+import SideBar from '../Sidebar';
+const Layout = () => {
+    return (<>
+        <SideBar>
+
+        
+        </SideBar>
+        </>
+    )
+}
+
+export default Layout
