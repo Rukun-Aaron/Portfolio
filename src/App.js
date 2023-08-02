75bb6e36455e29cb3147c0a04eab1a5d1ce1cd1f
BuildTools <raar518@aucklanduni.ac.nz>
2023-08-03 07:05:37 +1200

    changed basename

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 function App() {
   return (
     <>
-    <Routes>
+    <Routes basename={process.env.PUBLIC_URL}>
       <Route path="/" element={<Layout/>} >
         <Route index element={<Home/>} />
       </Route>
