4cd2be027ae2b8cbeb15cd3157b78808ef6df2e5
BuildTools <raar518@aucklanduni.ac.nz>
2023-08-03 04:05:44 +1200

    changed home path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout/>} >
+      <Route path="/Portfolio" element={<Layout/>} >
         <Route index element={<Home/>} />
       </Route>
     </Routes>
