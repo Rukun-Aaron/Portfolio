d5183796df7e7d0708607e8c3f8a767be5737ae4
BuildTools <raar518@aucklanduni.ac.nz>
2023-08-02 23:19:27 +1200

    created from page template

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,22 @@
+import LogoSubtitle from '../../assets/images/logo-sub-transparent.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
 const Home =() => {
-
-    return (<div></div>
-        // <div className="container home-page">
+    return (<>
+        <div className="container home-page">
             
-        //     <div className="text-zone"></div>
-        //         <h1>Hi, <br/> I'm</h1>
-        //         <img src={none} alt="devloper"/>
-        //     </div>
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm
+                <img src={LogoSubtitle} alt="devloper"/>
+                Aaron
+                <br/>
+                Computer Science Graduate
+                </h1>
+                <h2>Backend Developer/ Comfortable w JavaScript/ Machine Learning expert </h2>
+                <Link to='/contact' className="flat-button">CONTACT ME</Link>
+                </div>
+            </div>
+            </>
     )
 }
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -6,8 +6,12 @@ const Layout = () => {
     <div className='App'>
         <SideBar/>
         <div className='page'> 
-            <span className='tags top-tags'> &lt;body&gt;</span>
+            <span className='tags top-tags'> &lt;html&gt;
+             <br/>
+            <span className='top-tag-html'> &lt;body&gt;</span>
+            </span>
             <Outlet/>
+            
             <span className='tags bottom-tags'> &lt;/body&gt;
             <br/>
             <span className='bottom-tag-html'> &lt;/html&gt;</span>
