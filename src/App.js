d85cdc5fd97ce9ca671bdcbb8c235a0b5c1fbf9c
rukun aaron <raar518@aucklanduni.ac.nz>
2023-08-19 18:19:09 +1200

    changed cname

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <>
     <Routes basename={process.env.PUBLIC_URL}>
       <Route path="/" element={<Layout/>} >
+        
         <Route index element={<Home/>} />
       </Route>
     </Routes>
