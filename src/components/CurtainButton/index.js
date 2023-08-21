83131d033cd6e507483e457199930aaf2a6ebf58
Rukun <raar518@auckland.ac.nz>
2023-08-21 19:47:49 +1200

    added curtainbutton

--- src/components/CurtainButton/index.js
+++ src/components/CurtainButton/index.js
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './index.scss';
 import React  from "react";
+import 'font-awesome/css/font-awesome.min.css';
+// import {faXmarkLarge} from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
+import {faXmark} from '@fortawesome/free-solid-svg-icons';
+// import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 const CurtainButton = () => {
@@ -11,17 +16,24 @@ const CurtainButton = () => {
       setIsNavOpen(prevState => !prevState);
     };
     return (
+      // <div className="curtain-button" data-nav={isNavOpen}>
         <button
-          data-nav={isNavOpen}
+        data-nav={isNavOpen}
           id="nav-toggle"
           type="button"
           onClick={toggleNav}
-        >
-          <FontAwesomeIcon className='open' icon= " fa-light fa-xmark-large"/>
-          <FontAwesomeIcon className='close' icon= "fa-light fa-xmark-large"/>
-          {/* <i className="open fa-light fa-bars-staggered"></i>
-          <i className="close fa-light fa-xmark-large"></i> */}
+        > 
+          <FontAwesomeIcon icon={faXmark} />
+          <FontAwesomeIcon icon={faCoffee} />
+          
         </button>
+      // </div>
+    //   <div className={`curtain-button ${isNavOpen ? 'open' : ''}`}>
+    // <button id="nav-toggle" type="button" onClick={toggleNav}>
+    //     <FontAwesomeIcon icon={faXmark} className="icon-x" />
+    //     <FontAwesomeIcon icon={faCoffee} className="icon-coffee" />
+    // </button>
+// </div>
       );
 }
 
