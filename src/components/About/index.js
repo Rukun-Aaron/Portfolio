d15eacd1db0bd1cfe02077c5e3956a9d4c664593
rukun aaron <raar518@aucklanduni.ac.nz>
2023-08-27 01:02:41 +1200

    added about me page

---
+++ src/components/About/index.js
@@ -0,0 +1,30 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'    
+import { useState } from 'react';
+const About = ()=>{
+    const [letterClass, setLetterClass] = useState('text-animate')
+    return(
+        <div className="container about-page">
+        
+            <div className='text-zone'>
+            <h1>
+                <AnimatedLetters 
+                strArray={['A','b','o','u','t',' ','M','e']}
+                idx={15}
+                >  </AnimatedLetters>
+            </h1>  
+            <p>
+                    
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+            </p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+
+            </p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p> 
+            </div> 
+            
+            
+        </div>
+    )
+};
+export default About;
