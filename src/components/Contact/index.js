56ca5d607c5b6434751609d7d83d5caab5b90d57
rukun aaron <raar518@aucklanduni.ac.nz>
2023-08-30 01:19:11 +1200

    added tailwind

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 const Contact = () =>{
     const contactText = ["C", "o","n","t","a","c","t"," ","M","e"]
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -31,6 +31,29 @@ const Contact = () =>{
 
             </p>
             <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p> 
+                <div className='contact-form'>
+                    <form>
+                        <ul>
+                            <li className="half">
+                                <input type="text" name="name" placeholder="Name" required></input>
+                            </li>
+                            <li className="half">
+                                <input type="email" name="email" placeholder="Email" required></input>
+                            </li>
+                          <li>
+                            <input placeholder="Subject" type="text" name="subject" required/>
+                          </li>
+                          <li>
+                            <textarea placeholder='Message' name="message" required></textarea>
+                          </li>
+                          <li>
+                            <input type="submit" value="Send Message" className='flat-button' ></input>
+                          </li>
+                          
+
+                        </ul>
+                    </form>
+                </div>
                 </div>
             </div>
             <Loader type="pacman"/> 
